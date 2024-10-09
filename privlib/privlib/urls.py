91c24924bcffa76a04fe003a0b08fdfe3338e16b
rako <=>
2024-10-10 01:12:23 +0900

    bookアプリケーションのurl,view,templateを作成して連携させてトップページ表示した

--- privlib/privlib/urls.py
+++ privlib/privlib/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include #include関数をインポート
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('book.urls')), #bookアプリケーションのURL設定を読み込む、bookアプリケーションのurls.pyに処理を移譲する
 ]
