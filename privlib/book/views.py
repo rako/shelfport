23070fc0526893e8cb08eed0ff9c708206fe7b98
rako <=>
2024-10-11 03:41:55 +0900

    base.htmlに共通テンプレートを作成。CSS追加。settingsにstaticルーティング追加

--- privlib/book/views.py
+++ privlib/book/views.py
@@ -3,4 +3,4 @@ from django.views import generic
 
 # Create your views here.
 class IndexView(generic.TemplateView):
-    template_name = 'index.html'
+    template_name = 'index.html' #テンプレートの表示に特化したビュークラスのクラス変数（template_name）を設定、アプリ用ディレクトリ内のtemplatesディレクトリ内から探す
