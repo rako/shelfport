bf10cb2c1c27563e8cf2a87eb7171b1a7f48a5de
rako <rakoinfotech@gmail.com>
2024-10-15 14:37:30 +0900

    urlpatternsにinquiryを追加

--- privlib/book/urls.py
+++ privlib/book/urls.py
@@ -9,4 +9,5 @@ app_name = 'book'
 #「name=」では、アプリケーション用urls.py内で名前の衝突を避けるため
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('inquiry/', views.InquiryView.as_view(), name='inquiry'),
 ]
