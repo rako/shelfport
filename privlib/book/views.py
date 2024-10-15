460ad3015848f521303d3f978c5bdbdba337e214
rako <rakoinfotech@gmail.com>
2024-10-15 14:39:02 +0900

    viewsにInquiryViewを追加

--- privlib/book/views.py
+++ privlib/book/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
 
+from .forms import InquiryForm
+
 # Create your views here.
 class IndexView(generic.TemplateView):
     template_name = 'index.html' #テンプレートの表示に特化したビュークラスのクラス変数（template_name）を設定、アプリ用ディレクトリ内のtemplatesディレクトリ内から探す
+
+class InquiryView(generic.FormView):
+    template_name = "inqiry.html"
+    form_class = InquiryForm
