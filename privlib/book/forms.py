81cf44eeff2b9fe2be11dbcc8356cdb841bc50c6
rako <=>
2024-10-17 01:56:55 +0900

    fromをformとtypofixed

--- privlib/book/forms.py
+++ privlib/book/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 
-class InquiryFrom(forms.From):
+class InquiryForm(forms.Form):
     name = forms.CharField(label='お名前', max_length=30)
     email = forms.EmailField(label='メールアドレス')
     title = forms.CharField(label='タイトル', max_length=30)
