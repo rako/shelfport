809d2782af1556b0aa873006a526642151802544
rako <=>
2024-10-18 04:35:51 +0900

    loggerの追加と、InquiryViewでform_validのオーバーライド

--- privlib/book/views.py
+++ privlib/book/views.py
@@ -3,6 +3,12 @@ from django.views import generic
 
 from .forms import InquiryForm
 
+import logging
+
+from djang.urls import reverse_lazy
+
+logger = logging.getLogger(__name__)
+
 # Create your views here.
 class IndexView(generic.TemplateView):
     template_name = 'index.html' #テンプレートの表示に特化したビュークラスのクラス変数（template_name）を設定、アプリ用ディレクトリ内のtemplatesディレクトリ内から探す
@@ -10,3 +16,9 @@ class IndexView(generic.TemplateView):
 class InquiryView(generic.FormView):
     template_name = "inquiry.html"
     form_class = InquiryForm
+    success_url = reverse_lazy('book:inquiry') #reverse_lazy関数を使って、URLパターンの名前を指定している。ハードコーディングを避けるため。
+
+    def form_valid(self, form):
+        form.send_email()
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
