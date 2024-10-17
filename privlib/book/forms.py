be7bd50c662408b547ffb95f0b782443ba19b501
rako <=>
2024-10-18 03:11:49 +0900

    メッセージに対するコメントの追加

--- privlib/book/forms.py
+++ privlib/book/forms.py
@@ -5,7 +5,7 @@ class InquiryForm(forms.Form):
     name = forms.CharField(label='お名前', max_length=30)
     email = forms.EmailField(label='メールアドレス')
     title = forms.CharField(label='タイトル', max_length=30)
-    message = forms.CharField(label='メッセージ', widget=forms.Textarea)
+    message = forms.CharField(label='メッセージ', widget=forms.Textarea) #CharFieldはシングルlineのテキスト入力なので、widget=forms.Textareaで複数行のテキスト入力場所を表示。
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
