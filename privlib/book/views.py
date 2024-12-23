af611e0b2c9c02776c0e04a362aaa1b22e837797
rako <rakoinfotech@gmail.com>
2024-12-23 11:08:29 +0900

    送信完了のメッセージを画面表示した

--- privlib/book/views.py
+++ privlib/book/views.py
@@ -7,6 +7,8 @@ import logging
 
 from django.urls import reverse_lazy
 
+from  django.contrib import messages
+
 logger = logging.getLogger(__name__)
 
 # Create your views here.
@@ -20,5 +22,6 @@ class InquiryView(generic.FormView):
 
     def form_valid(self, form):
         form.send_email()
+        messages.success(self.request, 'メッセージを送信しました。')
         logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
         return super().form_valid(form)
